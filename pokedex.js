664955ed106794111f37ea3f7857c478ea78c4ea
Rociliu <mrrosa.carrasco@gmail.com>
2023-01-15 18:29:41 +0100

    a falta de revisar el estilo y la función map

--- pokedex.js
+++ pokedex.js
@@ -1,25 +1,71 @@
-const pokeList$$ = document.querySelector('#pokedex');
-//console.log(pokeList$$);
-const getPokemon = async () => {
-    const pokemonList = []
 
-    for (let i=1; i<151; i++) {
+const getPokemon = async () => {
+    for (let i = 1; i < 151; i++) {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
-        const data = await response.json();
-        console.log(data);
-        pokemonList.push({
-            id: data.id,
-            name: data.name,
-            image: data.sprites['front_default'],
-            type: data.types.map((type) => type.type.name).join(', '),
-        });
+        const pokemonCompleto = await response.json();
+        console.log(pokemonCompleto);
+        //hacer comentario mapeo
+        const pokemon = {
+            name: pokemonCompleto.name,
+            image: pokemonCompleto.sprites['front_default'],
+            type: pokemonCompleto.types.map((type) => type.type.name).join(', '),
+            id: pokemonCompleto.id
+        };
+
+        const pokeList$$ = document.querySelector('#pokedex');
+        //console.log(pokeList$$);
+
+        pintar(pokemon, pokeList$$);
     }
-    return pokemonList;
 }
-console.log(getPokemon());
+
+function pintar(pokemon, olDOM) {
+    const newLi = document.createElement('li');
+
+    const card = document.createElement('div');
+    newLi.className = 'card';
+    
+    const img = document.createElement('img');
+    img.src = pokemon.image;
+
+    const nombre = document.createElement('div');
+    nombre.className = "card-title";
+    nombre.innerHTML = pokemon.name;
+
+    const meta = document.createElement('div');
+    meta.className = "card-meta";
+
+    const id = document.createElement('div');
+    id.className = "card-id";
+    id.innerHTML = pokemon.id;
+
+    const type = document.createElement('div');
+    type.className = "card-type";
+    type.innerHTML = pokemon.type;
+
+    const innerCard = document.createElement('div');
+    innerCard.className = "card-innerCard";
+    
+    meta.appendChild(id);
+    meta.appendChild(type)
+    card.appendChild(meta);
+
+    innerCard.appendChild(img);
+    innerCard.appendChild(nombre);
+
+    card.appendChild(innerCard)
+
+
+    newLi.appendChild(card);
+
+    olDOM.appendChild(newLi);
+}
+
 
 const init = async () => {
     const pokemons = await getPokemon();
+
+
     
 }
 init();
