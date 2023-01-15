29d439af77ac2ef9101c2c4e3c96d87385fc2532
Rociliu <mrrosa.carrasco@gmail.com>
2023-01-15 18:30:21 +0100

    finalizado estilo

--- pokedex.js
+++ pokedex.js
@@ -1,9 +1,13 @@
+const pokeList$$ = document.querySelector('#pokedex');
+  //console.log(pokeList$$);
+
 
 const getPokemon = async () => {
     for (let i = 1; i < 151; i++) {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
         const pokemonCompleto = await response.json();
         console.log(pokemonCompleto);
+
         //hacer comentario mapeo
         const pokemon = {
             name: pokemonCompleto.name,
@@ -12,17 +16,19 @@ const getPokemon = async () => {
             id: pokemonCompleto.id
         };
 
-        const pokeList$$ = document.querySelector('#pokedex');
-        //console.log(pokeList$$);
-
-        pintar(pokemon, pokeList$$);
+        //llamo a la funcion dentro del bucle para que se pinte por cada vuelta.
+        pintarPokemon(pokemon, pokeList$$);
+        
     }
 }
 
-function pintar(pokemon, olDOM) {
+function pintarPokemon(pokemon, pokeList$$) {
+
+    //Creo los elementos en el DOM:
     const newLi = document.createElement('li');
 
-    const card = document.createElement('div');
+    
+    const card = document.createElement('div'); //Cada li será un div que llevará el contenido del pokemon en forma de carta
     newLi.className = 'card';
     
     const img = document.createElement('img');
@@ -32,8 +38,8 @@ function pintar(pokemon, olDOM) {
     nombre.className = "card-title";
     nombre.innerHTML = pokemon.name;
 
-    const meta = document.createElement('div');
-    meta.className = "card-meta";
+    const cardHeader = document.createElement('div');
+    cardHeader.className = "card-header";
 
     const id = document.createElement('div');
     id.className = "card-id";
@@ -43,30 +49,26 @@ function pintar(pokemon, olDOM) {
     type.className = "card-type";
     type.innerHTML = pokemon.type;
 
-    const innerCard = document.createElement('div');
-    innerCard.className = "card-innerCard";
+    const cardContent = document.createElement('div');
+    cardContent.className = "card-content";
     
-    meta.appendChild(id);
-    meta.appendChild(type)
-    card.appendChild(meta);
+    
+    cardHeader.appendChild(id);
+    cardHeader.appendChild(type)
+    card.appendChild(cardHeader);
 
-    innerCard.appendChild(img);
-    innerCard.appendChild(nombre);
-
-    card.appendChild(innerCard)
+    cardContent.appendChild(img);
+    cardContent.appendChild(nombre);
+    card.appendChild(cardContent);
 
 
     newLi.appendChild(card);
-
-    olDOM.appendChild(newLi);
+    pokeList$$.appendChild(newLi);
 }
 
 
 const init = async () => {
-    const pokemons = await getPokemon();
-
-
-    
+    const pokemons = await getPokemon();   
 }
 init();
 
